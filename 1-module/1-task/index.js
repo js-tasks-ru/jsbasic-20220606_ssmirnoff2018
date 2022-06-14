af4c0ad511e13d8356aa00ea8d5d60b402cf5251
Nikolay <ssmirnoff2018@yandex.ru>
2022-06-14 19:36:52 +0300

    fixed(module-1): solution task 1

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -1,9 +1,13 @@
 function factorial(n) {
   
-  if (n == 0 || n == 1) return 1;
-  
-  for (i = 0; i < n - 2; i++) {
-    n = n * (n - (i + 1));
+  let result = 1;
+  if (n === 0 || n === 1) {
+    return result;
   }
-  return n;
+
+  for (let i = 1; i <= n; i++) {
+    result = result * i;
+  }
+
+  return result;
 }
